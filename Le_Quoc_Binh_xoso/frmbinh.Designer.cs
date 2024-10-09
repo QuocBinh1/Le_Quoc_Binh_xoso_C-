2750d3f520dc7fe905dd80cf03501fc1c573e90c
QuocBinh1 <BinhPlayll@gmail.com>
2024-10-09 17:58:41 +0700

    Add project files.

---
+++ Le_Quoc_Binh_xoso/frmbinh.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Le_Quoc_Binh_xoso
+{
+    partial class frmbinh
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnplay = new System.Windows.Forms.Button();
+            this.lbg8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(10, 610);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(83, 32);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "G.ĐB";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(33, 525);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 32);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "G.1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(33, 441);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 32);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "G.2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(33, 365);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 32);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "G.3";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(33, 292);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 32);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "G.4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(33, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 32);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "G.6";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(33, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 32);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "G.7";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(33, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 32);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "G.5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 32);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "G.8";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(248, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 32);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Xổ Số Quảng Bình";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(6, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1007, 54);
+            this.textBox1.TabIndex = 10;
+            // 
+            // btnplay
+            // 
+            this.btnplay.Location = new System.Drawing.Point(532, 87);
+            this.btnplay.Name = "btnplay";
+            this.btnplay.Size = new System.Drawing.Size(75, 23);
+            this.btnplay.TabIndex = 21;
+            this.btnplay.Text = "Quay Số";
+            this.btnplay.UseVisualStyleBackColor = true;
+            this.btnplay.Click += new System.EventHandler(this.btnplay_Click);
+            // 
+            // lbg8
+            // 
+            this.lbg8.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.lbg8.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbg8.Location = new System.Drawing.Point(139, 78);
+            this.lbg8.Name = "lbg8";
+            this.lbg8.Size = new System.Drawing.Size(358, 32);
+            this.lbg8.TabIndex = 22;
+            this.lbg8.Text = "00";
+            this.lbg8.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // frmbinh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1179, 683);
+            this.Controls.Add(this.lbg8);
+            this.Controls.Add(this.btnplay);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "frmbinh";
+            this.Text = "frmbinh";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnplay;
+        private System.Windows.Forms.Label lbg8;
+    }
+}
---
+++ Le_Quoc_Binh_xoso/frmbinh.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Le_Quoc_Binh_xoso
+{
+    public partial class frmbinh : Form
+    {
+        public frmbinh()
+        {
+            InitializeComponent();
+        }
+        private int randomnumber(int min = 01, int max = 99)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+        private void btnplay_Click(object sender, EventArgs e)
+        {
+            int min = Properties.Settings.Default.numberMin;
+            int max = Properties.Settings.Default.numberMax;
+            int numresult = randomnumber(min,max);
+            lbg8.Text = numresult.ToString();
+             
+        }
+    }
+}
